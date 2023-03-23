df4789a6ec932859b934be1c6b2c224550f8a8cd
akubosylva <akubosilver@gmail.com>
2023-03-23 09:43:04 +0100

    Modify file

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-'''
-    Implementation of the Amenity class
-'''
-from os import getenv
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.place import place_amenity
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    '''
-        Implementation for the Amenities.
-    '''
-    __tablename__ = "amenities"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    """
+    Amenities Class representing amenities a place
+    can offer
+    """
+    __tablename__ = 'amenities'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity,
-                                       back_populates="amenities")
     else:
         name = ""
